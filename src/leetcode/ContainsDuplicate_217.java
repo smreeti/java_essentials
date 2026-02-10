ecb294db40728acbd42e2250d1138dccc7de8c6c
smriti.mool <mool.smreeti@gmail.com>
2026-02-09 21:47:02 -0500

    leetcode directory

---
+++ src/leetcode/ContainsDuplicate_217.java
@@ -0,0 +1,58 @@
+package leetcode;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+
+/*
+* Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+Example 1:
+
+Input: nums = [1,2,3,1]
+Output: true
+Explanation:
+The element 1 occurs at the indices 0 and 3.
+
+Example 2:
+Input: nums = [1,2,3,4]
+Output: false
+Explanation:
+All elements are distinct.
+
+Example 3:
+Input: nums = [1,1,1,3,3,4,3,2,4,2]
+Output: true
+* */
+public class ContainsDuplicate_217 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Enter the total number of inputs: ");
+        int numLength = Integer.parseInt(bufferedReader.readLine());
+        System.out.println("\n");
+
+        int[] numbers = new int[numLength];
+
+        for (int i = 0; i < numLength; i++) {
+            System.out.println("Enter number " + (i + 1) + ":");
+            numbers[i] = Integer.parseInt(bufferedReader.readLine());
+        }
+
+        HashSet<Integer> numList = new HashSet<>();
+        boolean hasDuplicates = false;
+
+        for (int i= 0; i < numbers.length; i++) {
+            if(numList.contains(numbers[i])) {
+              hasDuplicates = true;
+              break;
+            }
+
+            numList.add(numbers[i]);
+        }
+
+        System.out.println("Has duplicates : " + hasDuplicates);
+    }
+
+}
---
+++ src/leetcode/ValidAnagram_242.java
@@ -0,0 +1,71 @@
+package leetcode;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Objects;
+
+/*
+* Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+Example 1:
+Input: s = "anagram", t = "nagaram"
+Output: true
+
+Example 2:
+Input: s = "rat", t = "car"
+Output: false
+
+ */
+public class ValidAnagram_242 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.print("Enter first string: ");
+        String s = bufferedReader.readLine();
+
+        System.out.print("Enter second string: ");
+        String t = bufferedReader.readLine();
+
+        // Remove spaces & normalize case
+        s = s.replaceAll("\\s+", "").toLowerCase();
+        t = t.replaceAll("\\s+", "").toLowerCase();
+
+        if (s.length() != t.length()) {
+            System.out.println("Not an anagram");
+            return;
+        }
+
+        //Option 1
+//        char[] sArr = s.toCharArray();
+//        char[] tArr = t.toCharArray();
+//
+//        Arrays.sort(sArr);
+//        Arrays.sort(tArr);
+//
+//        boolean isAnagram = Arrays.equals(sArr, tArr);
+//
+//        System.out.println(isAnagram ? "Anagram" : "Not an anagram");
+
+
+        //Option 2 This will throw ArrayIndexOutOfBoundsException if input contains anything outside a–z.
+        int[] charCounts = new int[26];
+        boolean isAnagram = true;
+
+        for (int i = 0; i < s.length(); i++) {
+            charCounts[s.charAt(i) - 'a'] ++;
+            charCounts[t.charAt(i) - 'a'] --;
+        }
+
+        for (int count : charCounts) {
+            if (count != 0) {
+                isAnagram = false;
+                break;
+            }
+        }
+
+        System.out.println(isAnagram ? "Anagram" : "Not an anagram");
+
+    }
+}
